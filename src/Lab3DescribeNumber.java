3d4e1237c4c2d84e63f92db8d98f53b4e64f4097
Brian Eazer <brianeazer2015@gmail.com>
2018-10-09 15:03:52 -0400

    Second commit with Extra Challenges added on

--- src/Lab3DescribeNumber.java
+++ src/Lab3DescribeNumber.java
@@ -4,36 +4,67 @@ public class Lab3DescribeNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		input();
-	}
-	private static void input() {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		printer("What's your name?");
 		String name = sc.nextLine();
-		printer("OK, " + name +". Enter a number between 1 and 100");
-		int num = sc.nextInt();
-		tellMeAboutTheNumber(num);
+		input(name);
+		
 	}
-
-	public static void tellMeAboutTheNumber(int num) {
+	
+	private static void input(String name) {
 		// TODO Auto-generated method stub
-		if (num % 2 != 0 && num <= 59) {
-			printer("Odd");
+		int num = 0;
+		Scanner sc = new Scanner(System.in);
+		printer(name + ", give me a number.");
+		if (sc.hasNextInt()) {
+			num = sc.nextInt();
+			if (num >= 1 && num <= 100) {
+			tellMeAboutTheNumber(num, name);
+			}
+			else {
+				printer(name + ", your number is out of range.");
+				input(name);
+			}
 		}
-		else if (num >= 2 && num <= 25) {
-			printer("Even and less than 25");
-		}
-		else if (num >= 26 && num % 2 == 0) {
-			printer("even");
-		}
-		else {
-			printer("Odd and over 60");
-		}
-		input();
 		
 	}
 
+
+	public static void tellMeAboutTheNumber(int num, String name) {
+		// TODO Auto-generated method stub
+		if (num % 2 != 0 && num <= 59) {
+			printer(name + ", your number '" + num + "' is odd");
+		}
+		else if (num >= 2 && num <= 25) {
+			printer(name + ", your number is even and less than 25");
+		}
+		else if (num >= 26 && num <= 60 && num % 2 == 0) {
+			printer(name + ", your number is even");
+		}
+		else if (num > 60 && num % 2 == 0) {
+			printer(name + ", your number '" + num + "' is even and over 60");
+		}
+		else {
+			printer(name + ", your number '" + num + "' is odd and over 60");
+		}
+		repeat(name);
+	}
+	
+	public static void repeat(String name) {
+		Scanner sc = new Scanner(System.in);
+		printer("Do you want to continue? Type y/n");
+		String answer = sc.nextLine();
+		if (answer.equals("y")) {
+			input(name);
+		}
+		else if (answer.equals("n")) {
+				printer("Alright, it's been real " + name + ".");
+			}
+		else {
+			printer("ur smart. Here's some money. Go buy yourself a jetski, get a good night's sleep. Try again");
+			repeat(name);
+		}
+	}
 	private static void printer(String string) {
 		// TODO Auto-generated method stub
 		System.out.println(string);
